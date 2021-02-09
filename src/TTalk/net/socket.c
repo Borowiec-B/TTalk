d7cd1db86418d693bca9623f9665e5637adabf00
throwaway47912 <throwaway47912@gmail.com>
2021-02-09 17:03:33 +0100

    Add some addrinfo queries (is_ip/tcp/tcpip)

--- src/TTalk/net/socket.c
+++ src/TTalk/net/socket.c
@@ -1,8 +1,29 @@
 #include "TTalk/net/socket.h"
 
 #include <assert.h>
+#include <netdb.h>
 #include <string.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
+static bool addrinfo_is_ip(const struct addrinfo* const ai) {
+	assert(ai);
+
+	return ((ai->ai_protocol == AF_INET) || (ai->ai_protocol == AF_INET6));
+}
+
+static bool addrinfo_is_tcp(const struct addrinfo* const ai) {
+	assert(ai);
+
+	return (ai->ai_socktype == SOCK_STREAM);
+}
+
+static bool addrinfo_is_tcpip(const struct addrinfo* const ai) {
+	assert(ai);
+
+	return (addrinfo_is_ip(ai) && addrinfo_is_tcp(ai));
+}
 
 void TT_TCPSocket_init(struct TT_TCPSocket* const s) {
 	assert(s);
