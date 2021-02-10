6dd99e45dcdb9e35699a328036cc8b9b7a526626
throwaway47912 <throwaway47912@gmail.com>
2021-02-10 15:06:24 +0100

    Fix missing TT_ prefixes in address.h

--- include/TTalk/net/address.h
+++ include/TTalk/net/address.h
@@ -13,9 +13,9 @@ struct TT_Address {
 	const char* port;
 };
 
-bool addrinfo_is_ip(const struct addrinfo* const ai);
-bool addrinfo_is_tcp(const struct addrinfo* const ai);
-bool addrinfo_is_tcpip(const struct addrinfo* const ai);
-struct addrinfo* find_first_tcpip_addrinfo(const struct addrinfo* const linked_list);
+bool TT_addrinfo_is_ip(const struct addrinfo* const ai);
+bool TT_addrinfo_is_tcp(const struct addrinfo* const ai);
+bool TT_addrinfo_is_tcpip(const struct addrinfo* const ai);
+struct addrinfo* TT_find_first_tcpip_addrinfo(const struct addrinfo* const linked_list);
 
 #endif
--- src/TTalk/net/address.c
+++ src/TTalk/net/address.c
@@ -7,30 +7,30 @@
 #include <sys/types.h>
 
 
-bool addrinfo_is_ip(const struct addrinfo* const ai) {
+bool TT_addrinfo_is_ip(const struct addrinfo* const ai) {
 	assert(ai);
 
 	return ((ai->ai_family == AF_INET) || (ai->ai_family == AF_INET6));
 }
 
-bool addrinfo_is_tcp(const struct addrinfo* const ai) {
+bool TT_addrinfo_is_tcp(const struct addrinfo* const ai) {
 	assert(ai);
 
 	return (ai->ai_socktype == SOCK_STREAM);
 }
 
-bool addrinfo_is_tcpip(const struct addrinfo* const ai) {
+bool TT_addrinfo_is_tcpip(const struct addrinfo* const ai) {
 	assert(ai);
 
-	return (addrinfo_is_ip(ai) && addrinfo_is_tcp(ai));
+	return (TT_addrinfo_is_ip(ai) && TT_addrinfo_is_tcp(ai));
 }
 
-struct addrinfo* find_first_tcpip_addrinfo(const struct addrinfo* const linked_list) {
+struct addrinfo* TT_find_first_tcpip_addrinfo(const struct addrinfo* const linked_list) {
 	assert(linked_list);
 
 	const struct addrinfo* checked_addrinfo = &linked_list[0];
 
-	while (!addrinfo_is_tcpip(checked_addrinfo)) {
+	while (!TT_addrinfo_is_tcpip(checked_addrinfo)) {
 		if (!checked_addrinfo->ai_next)
 			return NULL;
 
