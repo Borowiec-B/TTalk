1711d979cc0a633e26780449653c2a0918c534f4
throwaway47912 <throwaway47912@gmail.com>
2021-02-12 04:35:45 +0100

    Document TT_find_first_tcpip_addrinfo()

--- include/TTalk/net/address.h
+++ include/TTalk/net/address.h
@@ -18,6 +18,10 @@ struct TT_Address {
 bool TT_addrinfo_is_ip(const struct addrinfo* const ai);
 bool TT_addrinfo_is_tcp(const struct addrinfo* const ai);
 bool TT_addrinfo_is_tcpip(const struct addrinfo* const ai);
+
+/*
+ *  TT_find_first_tcpip_addrinfo(): Return pointer to first found node with a TCP/IP sockaddr, or NULL if not found.
+ */
 struct addrinfo* TT_find_first_tcpip_addrinfo(const struct addrinfo* const node);
 TT_Status TT_sockaddr_ip_to_str(const struct sockaddr* const sa, char* buf, const socklen_t buf_size);
 
